95f8d4cae5ed79b66f9c4a362c716790dd493dd5
nitinsheoran88 <nitinsheoran47@gmail.com>
2025-02-18 00:11:55 +0530

    project update

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('js file is working.....')
+let modal = document.querySelector('.container-modal');
+
+function open_box() {
+    modal.style.display = 'flex';
+}
+
+function close_box() {
+    modal.style.display = 'none';
+}
+let Modal = document.querySelector('.container-modal1');
+function open_box1() {
+    Modal.style.display = 'flex';
+}
+
+function close_box1() {
+    Modal.style.display = 'none';
+}
